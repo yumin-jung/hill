0560fd30879b016d904964f16a2d6aa9515ce667
yumin-jung <jungym887@naver.com>
2022-12-19 15:13:24 +0900

    Add sun

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import {
 } from './hill.js';
 
 import {
+    Sun
+} from './sun.js';
+
+import {
     SheepController
 } from './sheep-controller.js';
 
@@ -12,6 +16,8 @@ class App {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext("2d");
         document.body.appendChild(this.canvas);
+
+        this.sun = new Sun();
 
         this.hills = [
             new Hill('#fd6bea', 0.2, 12),
@@ -37,6 +43,8 @@ class App {
         this.canvas.height = this.stageHeight * 2;
         this.ctx.scale(2, 2);
 
+        this.sun.resize(this.stageWidth, this.stageHeight);
+
         for (let i = 0; i < this.hills.length; i++) {
             this.hills[i].resize(this.stageWidth, this.stageHeight);
         }
@@ -48,6 +56,8 @@ class App {
         // Remove canvas
         requestAnimationFrame(this.animate.bind(this));
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+        this.sun.draw(this.ctx, t);
 
         let dots;
         for (let i = 0; i < this.hills.length; i++) {
